194d118669da7b8218b5de71f954e927dacaf7a0
Raul Velasquez <raul.velasquez@uto.edu.bo>
2022-04-29 18:08:40 -0400

    añadiendo prueba de lista de asociados

--- application/controllers/registro/Registro.php
+++ application/controllers/registro/Registro.php
@@ -5,13 +5,15 @@ class Registro extends CI_Controller
 {
   public function __construct()
   {
-    $this->load->model('Modelo_datos_estrategia1');
     parent::__construct();
+    $this->load->model('asociado_model');
   }
   public function index()
   {
+    $data['lista'] = $this->asociado_model->get_todos();
     $data['contenido'] = 'registro/formulario';
     $datosCapsula['data'] = $data;
+    print_r($data);
     $this->load->view('template/template', $datosCapsula);
   }
 }
